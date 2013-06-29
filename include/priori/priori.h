a07add7677abb9c3dd1ae3d5c6e4b12ab12cc865
John Farrier <farrier_john@bah.com>
2013-06-28 21:21:51 -0400

    Major performance improvement
    priori_cast<> now performs as well as or better than dynamic_cast<> in
    all tests.

--- include/priori/priori.h
+++ include/priori/priori.h
@@ -94,7 +94,7 @@ template<class T, class V> T priori_cast(V base)
 	if(base != nullptr)
 	{
 		// If it is convertable to the base class or to itself, return
-		if(std::is_convertible<std::add_pointer<V>::type, std::add_pointer<T>::type>::value == true)
+		if(std::is_convertible<std::remove_pointer<V>::type, std::remove_pointer<T>::type>::value == true)
 	    {
 			return reinterpret_cast<T>(base);
 		}
